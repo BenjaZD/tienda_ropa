528328c3497fab441e03be510b55cf752c6afd93
benjaZD <134301617+BenjaZD@users.noreply.github.com>
2024-04-10 20:17:31 -0400

    modal
    sas

---
+++ modal.js
@@ -0,0 +1,5 @@
+// Agrega la funcionalidad al botón "Ir al carrito"
+$('.btn-add-carrito').click(function() {
+    // Redirige al usuario a la página del carrito
+    window.location.href = 'carrito.html';
+  });
